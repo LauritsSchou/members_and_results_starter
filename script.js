cc33c43c3fb895735c80c188ab7835528f1d834b
LauritsSchou <laurits.schou@gmail.com>
2023-09-26 23:33:35 +0200

    displaying results

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let members = [];
 async function initApp() {
   initTabs();
   await getResults();
+  displayResults(results);
   // TODO: Make the rest of the program ...
 }
 async function getResults() {
@@ -16,5 +17,22 @@ async function getResults() {
   for (const resultData of resultJson) {
     const constructedResult = result.construct(resultData);
     results.push(constructedResult);
+  }
+}
+function displayResults(results) {
+  const resultTable = document.querySelector("#resultTableBody");
+  resultTable.innerHTML = "";
+  results.sort((a, b) => a.time - b.time);
+  for (const result of results) {
+    const resultTableHTML = /*html*/ `
+   <tr>
+   <td>${result.date}</td>
+   <td>${result.id}</td>
+   <td>${result.discipline}</td>
+   <td>${result.resultType}</td>
+   <td>${result.originalTime}</td>
+   </tr>
+    `;
+    resultTable.insertAdjacentHTML("beforeend", resultTableHTML);
   }
 }
