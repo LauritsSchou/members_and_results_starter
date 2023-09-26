675825fcccbc7a54f6864b712c7ba5b65ec6dc99
LauritsSchou <laurits.schou@gmail.com>
2023-09-26 23:21:42 +0200

    added results.json and fetch functionality

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ import * as result from "./results.js";
 import * as member from "./members.js";
 
 window.addEventListener("load", initApp);
-
+let results = [];
+let members = [];
 async function initApp() {
   initTabs();
-
+  await getResults();
   // TODO: Make the rest of the program ...
+}
+async function getResults() {
+  const response = await fetch("./results.json");
+  const resultJson = await response.json();
+  for (const resultData of resultJson) {
+    const constructedResult = result.construct(resultData);
+    results.push(constructedResult);
+  }
 }
